71c0f33e806c557223e821a3391159acace0e001
Dankrm <danton.mohr@gmail.com>
2023-04-01 10:34:58 -0300

    L2 - Criação do app e elemento p.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data () {
+        return {
+            product: 'Mala de viagem',
+            description: 'Mala com tamanho de 90x50x30cm'
+        }
+    }
+});
