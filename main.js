9d18b0664aea7fbb4231c77fa7eaddc5cd101a38
Danton Krieck Mohr <danton@areacentral.com.br>
2023-04-10 09:29:42 -0300

    L5 - Renderização de lista

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 1, color: 'green' },
+                { id: 2, color: 'blue' }
+            ],
+            sizes: [
+                { id: 1, name: 'Grande' },
+                { id: 2, name: 'Médio' },
+                { id: 3, name: 'Pequeno' }
+            ]
         }
     }
 })
